24c1e77b6b09e94ef2b18a424a7eeaac98885ee4
Johanne VIGNY <johanne.vigny@nexelec.fr>
2025-03-25 17:34:05 +0100

    feat: add script to init terminal with variable needed by zephyr

---
+++ env.cmd
@@ -0,0 +1,7 @@
+@echo off
+
+@REM set zephyr directory path in the env
+set ZEPHYR_BASE=C:\Users\JohanneVIGNY\zephyrproject1\zephyr
+
+@REM activate python virtual environment
+..\zephyrproject1\.venv\Scripts\activate.bat
