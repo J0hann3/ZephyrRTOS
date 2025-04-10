18fcadc47cc16d0fa490c76c40390d5929d3d769
Johanne VIGNY <johanne.vigny@nexelec.fr>
2025-04-10 14:15:53 +0200

    feat: add sleep mode to light sensor by cuting the power + remove global + pass pointer in function

--- zephyr_nxg_court/src/main.c
+++ zephyr_nxg_court/src/main.c
@@ -15,22 +15,18 @@
 #error "Unsupported board: sensor devicetree alias is not defined"
 #endif
 
-static const struct i2c_dt_spec lum_device = I2C_DT_SPEC_GET(I2C_LUM_NODE);
-static const struct i2c_dt_spec temp_device = I2C_DT_SPEC_GET(I2C_TEMP_NODE);
-static const struct gpio_dt_spec vcc_sensor = GPIO_DT_SPEC_GET(DT_ALIAS(vcc_sensor), gpios);
-
-uint16_t read_temp_sensor(const struct i2c_dt_spec temp_device, uint16_t *temp, uint16_t *humidity)
+uint8_t read_temp_sensor(const struct i2c_dt_spec *temp_device, uint16_t *temp, uint16_t *humidity)
 {
     uint8_t sensor_reg = 0xFD;
     uint8_t reading[6]= {0};
 
     // function i2c_write_read_dt doesn't generate a stop condition before switching to a read, no supported by all i2c sensor
-    if(i2c_write_dt(&temp_device, &sensor_reg, 1) != 0){
-        printf("Failed to write/read I2C device address %x at Reg. %x \n\r", temp_device.addr, sensor_reg);
+    if(i2c_write_dt(temp_device, &sensor_reg, 1) != 0){
+        printf("Failed to write/read I2C device address %x at Reg. %x \n\r", temp_device->addr, sensor_reg);
         return 1;
     }
     k_msleep(10);
-    i2c_read_dt(&temp_device, reading, 6);
+    i2c_read_dt(temp_device, reading, 6);
     (*temp) = reading[0] * 256 + reading[1];
     (*temp) = -45 + 175 * (*temp) / 65535;
 
@@ -41,47 +37,42 @@ uint16_t read_temp_sensor(const struct i2c_dt_spec temp_device, uint16_t *temp, 
 }
 
 
-int routine_read_temp(const struct i2c_dt_spec temp_device)
+int routine_read_temp(const struct i2c_dt_spec *temp_device)
 {
     uint16_t temp;
     uint16_t humidity;
 
     while (1)
     {
-        read_temp_sensor(temp_device, &temp, &humidity);
         k_msleep(1000);
+        read_temp_sensor(temp_device, &temp, &humidity);
     }
     return 0;
 }
 
-int init_lum_sensor(const struct i2c_dt_spec lum_device)
+int init_lum_sensor(const struct i2c_dt_spec *lum_device, const struct gpio_dt_spec *vcc_sensor)
 {
-    // Init Vcc sensor to then configure it
-    if (!gpio_is_ready_dt(&vcc_sensor))
-        return 1;
-    if (gpio_pin_configure_dt(&vcc_sensor, GPIO_OUTPUT_ACTIVE) < 0) 
+    // Init Vcc sensor to then configure i2c device(light sensor)
+    if (gpio_pin_configure_dt(vcc_sensor, GPIO_OUTPUT_ACTIVE) < 0) 
         return 1;
 
-    k_msleep(200);
-
-    if (!device_is_ready(lum_device.bus)) 
-        return 1;
+    k_msleep(5);
     
     uint8_t config[3] = {0x00, 0x00, 0x00}; // { commmand config, config LSB, config MSB }, config => gain 1, IT => 100ms, Pers => 1
 
-    if (i2c_write_dt(&lum_device, config, 3) != 0)
+    if (i2c_write_dt(lum_device, config, 3) != 0)
         return 1;
     return 0;
 }
 
-uint16_t read_lum_sensor(const struct i2c_dt_spec lum_device, uint16_t *light)
+uint8_t read_lum_sensor(const struct i2c_dt_spec *lum_device, uint16_t *light)
 {
     uint8_t sensor_reg = 0x04;
     uint8_t lum_reading[2]= {0};
 
-    int ret = i2c_write_read_dt(&lum_device, &sensor_reg, 1, &lum_reading[0], 2);
+    int ret = i2c_write_read_dt(lum_device, &sensor_reg, 1, &lum_reading[0], 2);
     if(ret != 0){
-        printf("Failed to write/read I2C device address %x at Reg. %x \n\r", lum_device.addr, sensor_reg);
+        printf("Failed to write/read I2C device address %x at Reg. %x \n\r", lum_device->addr, sensor_reg);
         return 1;
     }
     *light = ((uint16_t)lum_reading[1] << 8) + lum_reading[0];
@@ -89,7 +80,21 @@ uint16_t read_lum_sensor(const struct i2c_dt_spec lum_device, uint16_t *light)
     return 0;
 }
 
-int routine_read_lum(const struct i2c_dt_spec lum_device)
+uint8_t init_and_read_lum_sensor(const struct i2c_dt_spec *lum_device, const struct gpio_dt_spec *vcc_sensor, uint16_t *light)
+{
+	if (init_lum_sensor(lum_device, vcc_sensor) == 1){
+		printf("Init i2c error\n");
+		return 1;
+	}
+    k_msleep(150);
+	if (read_lum_sensor(lum_device, light))
+		return 1;
+	
+    gpio_pin_configure_dt(vcc_sensor, GPIO_INPUT | GPIO_PULL_UP);
+	return 0;
+}
+
+int routine_read_lum(const struct i2c_dt_spec *lum_device)
 {
     uint16_t light;
     while (1)
@@ -103,26 +108,28 @@ int routine_read_lum(const struct i2c_dt_spec lum_device)
 
 int main()
 {
+    const struct i2c_dt_spec lum_device = I2C_DT_SPEC_GET(I2C_LUM_NODE);
+    const struct i2c_dt_spec temp_device = I2C_DT_SPEC_GET(I2C_TEMP_NODE);
+    const struct gpio_dt_spec vcc_sensor = GPIO_DT_SPEC_GET(DT_ALIAS(vcc_sensor), gpios);
     uint16_t light;
     uint16_t temp;
     uint16_t humidity;
 
-    k_sleep(K_SECONDS(1));
+    k_msleep(50);
     if (!device_is_ready(temp_device.bus)) 
         return 1;
-    if (init_lum_sensor(lum_device) == 1){
-        printf("Init i2c error\n");
+    if (!device_is_ready(lum_device.bus)) 
         return 1;
-    }
+    if (!gpio_is_ready_dt(&vcc_sensor))
+        return 1;
 
     while (1)
     {
-        if (read_lum_sensor(lum_device, &light) == 1 )
+        k_msleep(1000);
+        if (init_and_read_lum_sensor(&lum_device, &vcc_sensor, &light) == 1)
             return 1;
-        if (read_temp_sensor(temp_device, &temp, &humidity) == 1 )
+        if (read_temp_sensor(&temp_device, &temp, &humidity) == 1)
             return 1;
-
-        k_sleep(K_SECONDS(1));
     }
     return 0;
 }
