320f2ac8b6884a608775a4907ad049099b4461ac
Johanne Vigny <johanne.vigny@nexelec.fr>
2025-06-06 14:01:22 +0200

    add jlink to flash configuration

--- zephyr/boards/atmel/NxG_Court/board.cmake
+++ zephyr/boards/atmel/NxG_Court/board.cmake
@@ -1,1 +1,8 @@
+board_set_debugger_ifnset(jlink)
+board_set_flasher_ifnset(jlink)
+
+board_runner_args(jlink "--device=ATSAMD21G18A")
+
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
