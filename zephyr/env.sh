6eadae6da9bf8feae8b1afb8061761905f1bbab3
Johanne Vigny <johanne.vigny@nexelec.fr>
2025-08-21 16:31:08 +0200

    add linux script to set variable and python virtual env

---
+++ zephyr/env.sh
@@ -0,0 +1,7 @@
+# Run the script with `source`, like `source env.sh`
+
+# set zephyr directory path in the env
+export ZEPHYR_BASE=C:/Users/JohanneVIGNY/zephyrproject1/zephyr
+
+# activate python virtual environment
+. ../../zephyrproject1/.venv/Scripts/activate
